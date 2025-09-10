e6239c5c2d194e1b0c165862163f48652531b87e
mamazu <14860264+mamazu@users.noreply.github.com>
2025-09-10 17:47:54 +0200

    Adding method rename refactoring for `getRole` method

--- config/sets/sulu/sulu-26.php
+++ config/sets/sulu/sulu-26.php
@@ -5,4 +5,15 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->ruleWithConfiguration(
+        RenameMethodRector::class,
+        [
+            // @see https://github.com/sulu/sulu/pull/8185
+            new MethodCallRename(
+                'Sulu\Bundle\SecurityBundle\Entity\Role',
+                'getRole',
+                'getIdentifier',
+            ),
+        ],
+    )
 };
